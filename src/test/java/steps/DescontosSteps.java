b155c1ee8c345025938ca7570711ca5a2dcf0bf5
ICTS - JANIER <janierfsilva@gmail.com.br>
2023-03-14 09:18:48 -0400

    correção de algumas linhas e utilizando novos comandos.

--- src/test/java/steps/DescontosSteps.java
+++ src/test/java/steps/DescontosSteps.java
@@ -1,31 +1,32 @@
 package steps;
 
-import cucumber.api.PendingException;
 import cucumber.api.java.pt.Dado;
 import cucumber.api.java.pt.Então;
 import cucumber.api.java.pt.Quando;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class DescontosSteps {
+    WebDriver driver = new ChromeDriver();
     @Dado("^que estou no site da qazando$")
-    public void acessar_site_qazando(){
-        System.out.println("Passou 01");
+    public void acessar_site_qazando() {
+        driver.get("https://qazando.com.br/curso.html");
 
     }
+
     @Quando("^eu preencho meu e-mail$")
     public void eu_preencho_meu_e_mail() {
-        System.out.println("Passou 02");
-
+        driver.findElement(By.id("email")).sendKeys("finotti@qazando.com.br");
     }
 
     @Quando("^clico em ganhar cupom$")
     public void clico_em_ganhar_cupom() {
-        System.out.println("Passou 03");
-
+        driver.findElement(By.id("button")).click();
     }
 
     @Então("^eu vejo o código de desconto$")
     public void eu_vejo_o_código_de_desconto() {
-        System.out.println("Passou 04");
 
     }
 }
